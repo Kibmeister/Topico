7cf4a24495bb6ff99b626279e4ad6ea2fd615985
F1aze <fkachorsens@gmail.com>
2019-04-29 19:40:27 +0200

    test

--- Raspberry/main.js
+++ Raspberry/main.js
@@ -12,14 +12,14 @@ var sound = new Sound({
 sound.record()
 console.log('recording started')
 
-setTimeout(function () {
-  sound.pause() // pause the recording after five seconds
-}, 5000)
+// setTimeout(function () {
+//   sound.pause() // pause the recording after five seconds
+// }, 5000)
 
-setTimeout(function () {
-  sound.resume() // and resume it two seconds after pausing
-}, 7000)
+// setTimeout(function () {
+//   sound.resume() // and resume it two seconds after pausing
+// }, 7000)
 
 setTimeout(function () {
   sound.stop() // stop after ten seconds
-}, 10000)
+}, 1000)
