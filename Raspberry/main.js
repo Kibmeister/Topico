f954a3cd199705c2d8f5353c44170b0034449d75
F1aze <fkachorsens@gmail.com>
2019-05-07 15:52:28 +0200

    ftp test

--- Raspberry/main.js
+++ Raspberry/main.js
@@ -1,3 +1,12 @@
 'use strict'
 
-var fs = require('fs')
+// var fs = require('fs')
+var Client = require('ftp')
+
+var c = new Client()
+c.on('ready', function () {
+  c.put('foo.txt', 'foo.remote-copy.txt', function (err) {
+    if (err) throw err
+    c.end()
+  })
+})
