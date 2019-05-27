641a658a9a69371537713afb7245f8d193aa0812
F1aze <fkachorsens@gmail.com>
2019-05-27 10:47:59 +0200

    hvbhijokp

--- projekt-v-1.0/server.js
+++ projekt-v-1.0/server.js
@@ -31,17 +31,13 @@ const upload = multer({
 })
 
 // Method to handle incoming file uploads:
-/*
 app.post(
   '/upload',
   upload.single('file'),
   (req, res) => {
-    //console.log('Requested file: ', req.file)
-    //console.log('Requested file path:', req.path)
-    // console.log('Requested file name:', req.name)
     var filePath = path.join(__dirname, req.path)
     fs.writeFileSync(filePath, req.file)
-    const targetPath = path.join(__dirname, './uploads/', req.originalname)
+    const targetPath = path.join(__dirname, './uploads/testVoice')
     console.log('Requested file: ', req.file)
     const tempPath = req.path
 
@@ -61,10 +57,10 @@ app.post(
       })
     }
   }
-) */
+)
 
 app.post('/uploadAudio', upload.single('file'), function (req, res) {
-  console.log(req.file)
+  console.log(req)
   let uploadLocation = path.join(__dirname, '/uploads/', req.file.originalname)
   // where to save the file to. make sure the incoming name has a .wav extension
 
