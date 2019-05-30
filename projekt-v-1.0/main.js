9c9fc97af2e9b8fc211af1249dfac3225ce68f2d
F1aze <fkachorsens@gmail.com>
2019-05-30 15:47:55 +0200

    vghjkl

--- projekt-v-1.0/main.js
+++ projekt-v-1.0/main.js
@@ -180,6 +180,7 @@ function queWord () {
   WordsClass.getQueWords(chosenWord.word, function (err, res) {
     if (err) return err
     console.log('Response: ', res)
+    queWords.push(res.queword1, res.queword2, res.queword3)
     console.log('Quewords: ', queWords)
     setTimeout(() => {
       for (var i = 0; i < res.length; i++) {
