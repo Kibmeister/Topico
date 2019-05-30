27a77d0372d961a4b7ab99c9b7a9e11071126481
F1aze <fkachorsens@gmail.com>
2019-05-30 10:54:41 +0200

    ghjkl

--- projekt-v-1.0/main.js
+++ projekt-v-1.0/main.js
@@ -132,7 +132,8 @@ function choose () {
   chosenWord = fourWords[Math.floor(Math.random() * fourWords.length)]
   console.log('Chosen word: ', chosenWord)
   LCDClass.clearAll()
-  chosenWord.lcd.println('You got:', 1, chosenWord.word, 2)
+  chosenWord.lcd.println('You got:', 1)
+  chosenWord.lcd.println(chosenWord.word, 2)
 }
 
 function choose2 () {
