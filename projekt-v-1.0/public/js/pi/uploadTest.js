402af471cac4c93c1b42c533674ab8dfe41ac9fc
F1aze <fkachorsens@gmail.com>
2019-06-03 13:05:11 +0200

    fghjk

--- projekt-v-1.0/public/js/pi/uploadTest.js
+++ projekt-v-1.0/public/js/pi/uploadTest.js
@@ -1,6 +1,5 @@
 const UploadFile = require('./upload.js').UploadFile
-const path = require('path')
 
-UploadFile.UploadFile(path.join(__dirname, 'temp/tempFile.wav'))
+UploadFile.UploadFile('./temp/tempFile.wav')
 
 // Assumes a server.js is running locally
