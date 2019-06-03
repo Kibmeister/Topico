5ea062672fe9229ba001bca8540e8dcf487664bf
F1aze <fkachorsens@gmail.com>
2019-06-03 13:33:11 +0200

    fghjk

--- projekt-v-1.0/public/js/pi/uploadTest.js
+++ projekt-v-1.0/public/js/pi/uploadTest.js
@@ -1,5 +1,6 @@
 const UploadFile = require('./upload.js').UploadFile
 const path = require('path')
+const filepath = path.join(__dirname, '/temp/tempFile.wav')
 
-UploadFile.UploadFile(path.join(__dirname, '/temp/tempFile.wav'))
+UploadFile.UploadFile(filepath)
 console.log('Attempting upload from: ', __dirname, '/temp/tempFile.wav')
