8b61b5f50cca996db19503dad9a9cef9a6279573
F1aze <fkachorsens@gmail.com>
2019-06-12 10:34:41 +0200

    vhjkl

--- projekt-v-1.0/buttontest.js
+++ projekt-v-1.0/buttontest.js
@@ -1,11 +1,14 @@
 'use strict'
 
 var Gpio = require('onoff').Gpio
-var pushButton1 = new Gpio(4, 'in', 'both', { debounceTimeout: 100 })
+var pushButton1 = new Gpio(4, 'in', 'both', { debounceTimeout: 3000 })
 
-pushButton1.watch(function (err, value) {
+// pushButton1.watch(function (err, value) {
+//   if (err) throw err
+//   console.log(value)
+// })
+
+pushButton1.readSync((err, res) => {
   if (err) throw err
-  if (value === 1) {
-    console.log(value)
-  }
+  console.log(res)
 })
