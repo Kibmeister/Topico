4b6106ee06d53636674e3aa9cc565b9ea0fce001
F1aze <fkachorsens@gmail.com>
2019-06-12 12:17:27 +0200

    vbhjk

--- projekt-v-1.0/buttontest.js
+++ projekt-v-1.0/buttontest.js
@@ -4,7 +4,7 @@ var Gpio = require('onoff').Gpio
 // var pushButton1 = new Gpio(4, 'in', 'rising', { debounceTimeout: 1000 })
 // Output is sent 1 second after button is released
 
-var pushButton1 = new Gpio(4, 'in', 'falling', { debounceTimeout: 1000 })
+var pushButton1 = new Gpio(4, 'in', 'falling', { debounceTimeout: 5000 })
 
 pushButton1.watch(function (err, value) {
   if (err) throw err
