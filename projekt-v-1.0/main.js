93cba434f945c6a0e3c0b9a6cbf7c176435d2567
F1aze <fkachorsens@gmail.com>
2019-06-14 10:32:00 +0200

    cvgjk

--- projekt-v-1.0/main.js
+++ projekt-v-1.0/main.js
@@ -37,7 +37,7 @@ var printHelpWords1, printHelpWords2, printHelpWords3
 
 var pushButton1 = new Gpio(4, 'in', 'rising', { debounceTimeout: 3000 })
 // Output is sent 1s after releasing the button
-var pushButton2 = new Gpio(14, 'in', 'falling', { debounceTimeout: 5000 })
+var pushButton2 = new Gpio(14, 'in', 'rising', { debounceTimeout: 5000, activeLow: true })
 // Output is sent after holding the button for 5 seconds
 var phase = 1
 
